92550a3d5e32694ae2fc3d403f975a221bf26336
ayumi saito <ayumisaito@ayumis-MBP.fritz.box>
2020-12-19 16:50:37 +0100

    add array todo list

--- src/App.js
+++ src/App.js
@@ -2,17 +2,22 @@ import './App.css';
 import React, { useState } from 'react';
 
 function App() {
-  const [todo, setTodo] = useState("");
-  console.log(todo)
+  const [todo, setTodo] = useState([{text : 'test'},{ item: []}]);
+
   const onSubmit = event => {
-    localStorage.setItem('todoLocalStorage', todo);
+    localStorage.setItem('todoLocalStorage', JSON.stringify(todo));
+  }
+  const getItem = event => {
+    localStorage.getItem('todoLocalStorage');
   }
 
+  console.log(localStorage.getItem('todoLocalStorage'))
   return (
     <div className="App">
       <form onSubmit={onSubmit}>
-        <input type="text" value={todo} onChange={(e) =>setTodo(e.target.value)} />
+        <input type="text" value={todo.text} onChange={(e) =>setTodo({ item: e.target.value})} />
       </form>
+
     </div>
   );
 }
