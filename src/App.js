6ad7cf91588ec9ce48d2acc7dc4ea13f6925d2aa
ayumi saito <ayumisaito@ayumis-MBP.fritz.box>
2020-12-21 12:08:38 +0100

    class to function, hooks rewrite

--- src/App.js
+++ src/App.js
@@ -1,32 +1,58 @@
 import './App.css';
-import React from 'react';
+import React, { useState }from 'react';
 
-class App extends React.Component {
- constructor(props) {
-   super(props);
-   this.state = {
-     text: '',
-     list: ['sample', 'sample2']
-   }
- }
+// class App extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       text: '',
+//       list: []
+//     }
+//   }
+//
+//   handleSubmit = (e) => {
+//     e.preventDefault();
+//     this.setState({
+//       list: [...this.state.list, this.state.text]
+//     })
+//   }
+//
+//   render() {
+//     return (
+//         <div>
+//           <form onSubmit={this.handleSubmit}>
+//             <input type='text'
+//                    onChange={(e) => this.setState({text: e.target.value})}
+//             />
+//           </form>
+//           {this.state.list.map((todo, i) => <li key={i}>{todo}</li>)}
+//         </div>
+//     )
+//   }
+// }
 
- handleSubmit = (e) =>{
-   e.preventDefault();
-   this.setState({ list:[...this.state.list, this.state.text]
-   })}
+function App (){
+  const [todo, setTodo] = useState("")
+  const [list, setList] = useState([])
 
-  render() {
-   return (
-       <div>
-        <form onSubmit={this.handleSubmit}>
-          <input type='text'
-                 onChange={(e) => this.setState({ text: e.target.value} )}
-              />
-        </form>
-         {this.state.list.map((todo, i) => <li key={i}>{todo}</li>)}
-       </div>
-   )
+  const handleSubmit =(e)=>{
+    e.preventDefault();
+    setList([todo, ...list]);
   }
+  const toggleTodo = id=> {
+     console.log(id)
+  }
+    return(
+   <div>
+   <form onSubmit={handleSubmit}>
+     <input type='text'
+            onChange={(e) => setTodo({text:e.target.value, completed: false})}
+            />
+   </form>
+     {list.map((todo, i) => <li key={i}
+     onClick={toggleTodo(i)}
+     >{todo.text}</li>)}
+   </div>
+    )
 }
-
 export default App;
